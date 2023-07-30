4dd381d9f7de5c54b1d4dc1503d8383c37cbc385
ivaquero <msaintms@outlook.com>
2023-07-30 17:40:44 +0800

    build: 🏗️ update build

---
+++ build/CMakeFiles/clock.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clock_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/clock_autogen.dir/ParseCache.txt"
+  "clock_autogen"
+  "CMakeFiles/clock.dir/clock.cpp.o"
+  "CMakeFiles/clock.dir/clock.cpp.o.d"
+  "CMakeFiles/clock.dir/clock_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/clock.dir/clock_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/clock.dir/main.cpp.o"
+  "CMakeFiles/clock.dir/main.cpp.o.d"
+  "clock"
+  "clock.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
